e88381081e51d489c9b4fb9b0e1fe6e88ef95979
Guilherme Rosa <gmoreira.rosa@icloud.com>
2020-07-07 06:51:16 -0300

    Efeito de troca de cenario

--- scripts/jogo/cenario.js
+++ scripts/jogo/cenario.js
@@ -3,48 +3,70 @@ class Cenario {
     this.imagens = imagens;
     this.velocidade = velocidade;
     this.indexImagemAtual = 0;
-    this.indexImagemFutura = 0;
     this.x1 = 0;
     this.x2 = width;
-    this.trocaCenario = false;
-    this.trocaCenarioFuturo = false;
+    this.aplicarEfeitoTrocaCenario = false;
+    this.deveTrocarCenario = false;
+    this.contadorEfeitoTrocaCenario = 0;
   }
 
   exibe() {
     image(this.imagens[this.indexImagemAtual], this.x1, 0, width, height);
-    image(this.imagens[this.indexImagemFutura], this.x2, 0, width, height);
+    image(this.imagens[this.indexImagemAtual], this.x2, 0, width, height);
+
+    if (this.aplicarEfeitoTrocaCenario) {
+      this.aplicarEfeitoTransacaoCenario();
+    }
+  }
+
+  aplicarEfeitoTransacaoCenario() {    
+    this.aplicarEfeitoTrocaCenario = true;
+    if (frameCount % 2 === 0) {
+      this.imagens[this.indexImagemAtual].filter(INVERT);
+      this.contadorEfeitoTrocaCenario += 1;
+    }
+  }
+
+  encerrarEfeitoTransacaoCenario() {
+    this.aplicarEfeitoTrocaCenario = false;
+    if (this.contadorEfeitoTrocaCenario % 2 !== 0) {
+      this.imagens[this.indexImagemAtual].filter(INVERT);
+    }
+    this.contadorEfeitoTrocaCenario = 0;
+  }
+
+  proximoNivel() {
+    this.aplicarEfeitoTransacaoCenario();
+    setTimeout(() => {
+      this.deveTrocarCenario = false;
+      this.encerrarEfeitoTransacaoCenario();
+      if (this.indexImagemAtual < this.imagens.length - 1) {
+        this.indexImagemAtual += 1;
+      } else {
+        this.indexImagemAtual = 0;
+      }
+    }, 400);
   }
 
   move() {
     this.x1 -= this.velocidade;
-    this.x2 -= this.velocidade;
+    this.x2 -= this.velocidade;    
     
     if (pontuacao.pontos > 0 && pontuacao.pontos.toFixed(2) % fita.configuracoes.pontuacaoNecessariaTrocaCenario === 0) {
-      this.trocaCenario = true;
+      this.deveTrocarCenario = true;
     }
 
     if (this.x1 < -width) {
-      if (this.trocaCenario) {
-        if (this.indexImagemAtual < this.imagens.length - 1) {
-          this.indexImagemAtual += 1;
-        } else {
-          this.indexImagemAtual = 0;
-        }        
-        this.trocaCenarioFuturo = true;
-        this.trocaCenario = false;
+      if (this.deveTrocarCenario) {
+        this.proximoNivel();
       }
       this.x1 = width;
     }
 
     if (this.x2 < -width) {
-        if (this.trocaCenarioFuturo) {
-          if (this.indexImagemFutura < this.imagens.length - 1) {
-            this.indexImagemFutura += 1;
-          } else {
-            this.indexImagemFutura = 0;
-          }
-          this.trocaCenarioFuturo = false;
-        }
+      if (this.deveTrocarCenario) {
+        this.proximoNivel();
+      }
       this.x2 = width;
     }
   }
